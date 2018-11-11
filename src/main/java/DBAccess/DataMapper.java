fa1f3392d2ee28c1fcbe7976b638d41cc7177961
jenzuffer <35786745+jenzuffer@users.noreply.github.com>
2018-11-11 14:43:58 +0100

    initial upload

---
+++ src/main/java/DBAccess/DataMapper.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBAccess;
+
+import FunctionLayer.Materials;
+
+/**
+ *
+ * @author Christian
+ */
+public class DataMapper {
+
+    public static Materials ChangeOrder(Materials materials, int OrderID) {
+       
+        return materials;
+    }
+    
+}
---
+++ src/main/java/FunctionLayer/LogicFacade.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FunctionLayer;
+import DBAccess.DataMapper;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Christian
+ */
+public class LogicFacade {
+
+    public static List<Materials> GetMaterials() {
+        List<Materials> TotalMaterials = new ArrayList();
+
+        return TotalMaterials;
+    }
+
+    public static Materials ChangeOrder(int OrderID, Materials materials) {
+        materials = DataMapper.ChangeOrder(materials, OrderID);
+        
+        return materials;
+    }
+}
